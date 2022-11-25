5ca4cdf516b91469f802cd9bc06e37de0bab8c83
Edison <edison.matos@live.com>
2022-11-25 20:26:42 -0300

    Updates

--- Calc/script/script.js
+++ Calc/script/script.js
@@ -1,23 +1,39 @@
 const mainInput = document.getElementById("mainInput")
 const resultInput = document.getElementById("resultInput")
+
 const buttonEqual = document.getElementById("buttonEqual")
 const buttonClear = document.getElementById("buttonClear")
 
 const buttonSum = document.getElementById("buttonSum")
+const buttonSlash = document.getElementById("buttonSlash")
+const buttonMinus = document.getElementById("buttonMinus")
+const buttonX = document.getElementById("buttonX")
+
 const button1 = document.getElementById("button1")
 const button2 = document.getElementById("button2")
-const buttonParen1 = document.getElementById("buttonParantesis1")
-const buttonParen2 = document.getElementById("buttonParantesis2")
+const button3 = document.getElementById("button3")
+const button4 = document.getElementById("button4")
+const button5 = document.getElementById("button5")
+const button6 = document.getElementById("button6")
+const button7 = document.getElementById("button7")
+const button8 = document.getElementById("button8")
+const button9 = document.getElementById("button9")
+
+buttonEqual.addEventListener('click', calculate)
+buttonClear.addEventListener('click', clearInputs)
+
+// const buttonParen1 = document.getElementById("buttonParantesis1")
+// const buttonParen2 = document.getElementById("buttonParantesis2")
 
 const buttonsList = []
-buttonsList.push(button1, button2, buttonSum, buttonParen1, buttonParen2)
+buttonsList.push(
+  button1, button2, button3, button4, button5, button6, button7, button8, button9, 
+  buttonSum, buttonSlash, buttonMinus, buttonX,
+  )
 
 for (i = 0; i < buttonsList.length; i++) {
   buttonsList[i].addEventListener('click', sendValueToMainInput)
 }
-
-buttonEqual.addEventListener('click', calculate)
-buttonClear.addEventListener('click', clearInputs)
 
 function sendValueToMainInput() {
   const sendValue = this.innerText
@@ -27,6 +43,7 @@ function sendValueToMainInput() {
 function calculate () {
   const result = eval(mainInput.value)
   resultInput.value = result
+  mainInput.value = ""
 }
 
 function clearInputs () {
